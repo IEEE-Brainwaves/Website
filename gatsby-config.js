6cbeed91c86655b30713b4b448185a8275f2324c
Anesh Kaul <aneshkaul@gmail.com>
2021-01-06 14:15:14 +0530

    fixed gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,11 +55,5 @@ module.exports = {
         ],
       },
     },
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        trackingId: config.googleAnalyticsID,
-      },
-    },
   ],
 };
